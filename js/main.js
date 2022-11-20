aa4f0e22a4da62bf7c6ccbc20428397c055d7d7e
justP-official <red-hot-shot@yandex.ru>
2022-11-20 16:08:51 +0300

    Добавление счётчика

--- js/main.js
+++ js/main.js
@@ -1,5 +1,29 @@
 "use stricts";
 
+let counter = 1;
+let counterText = document.querySelector(".switcher__counter");
+let add = document.querySelector(".switcher__button_add");
+let del = document.querySelector(".switcher__button_del");
+
+// console.log(counter);
+
+add.addEventListener("click", function() {
+    counter++;
+    counterText.innerHTML = counter;
+    console.log(counter);
+});
+
+del.addEventListener("click", function() {
+    counter--;
+    if (counter <= 0) {
+        counter = 1;
+    }
+    counterText.innerHTML = counter;
+    console.log(counter);
+});
+
+
+
 let result = document.querySelector(".text-box__text");
 
 let inputs = document.querySelectorAll(".form__item[type='number']");
