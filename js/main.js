2f9b6deb1c116fe8092d221a7e900fe0d7c66421
justP-official <red-hot-shot@yandex.ru>
2022-11-20 19:51:07 +0300

    Добавление функции создания нескольких теней

--- js/main.js
+++ js/main.js
@@ -2,27 +2,37 @@
 
 let counter = 1;
 let counterText = document.querySelector(".switcher__counter");
+let btn = document.querySelectorAll(".switcher__button");
 let add = document.querySelector(".switcher__button_add");
 let del = document.querySelector(".switcher__button_del");
+
 
 // console.log(counter);
 
 add.addEventListener("click", function() {
-    counter++;
-    counterText.innerHTML = counter;
-    console.log(counter);
+    if (counter == 3) {
+        counter = 3;
+    } else {
+        counter++;
+        counterText.innerHTML = counter;
+        console.log(counter);
+        document.querySelector(`.form${counter}`).style.display = "block";
+        document.querySelector(`.text-box__text${counter}`).style.display = "block";
+    }
+
 });
 
 del.addEventListener("click", function() {
-    counter--;
-    if (counter <= 0) {
+    if (counter == 1) {
         counter = 1;
+    } else {
+        document.querySelector(`.form${counter}`).style.display = "none";
+        document.querySelector(`.text-box__text${counter}`).style.display = "none";
+        counter--;
+        counterText.innerHTML = counter;
     }
-    counterText.innerHTML = counter;
     console.log(counter);
 });
-
-
 
 let result = document.querySelector(".text-box__text");
 
@@ -42,23 +52,51 @@ checkbox.forEach(function(checkboxs) {
 });
 
 function createShadow() {
-    let x = inputs[0].value;
-    let y = inputs[1].value;
-    let blurRadius = inputs[2].value;
-    let spreadRadius = inputs[3].value;
+    let x1 = inputs[0].value;
+    let y1 = inputs[1].value;
+    let blurRadius1 = inputs[2].value;
+    let spreadRadius1 = inputs[3].value;
 
-    let shadowColor = color[0].value;
+    let shadowColor1 = color[0].value;
 
-    let isInset = checkbox[0].value;
+    let isInset1 = checkbox[0].value;
     if (checkbox[0].checked) {
-        isInset = "inset";
+        isInset1 = "inset";
     }
 
-    let shadowBox = `${x}px ${y}px ${blurRadius}px ${spreadRadius}px ${isInset}`;
+    let x2 = inputs[4].value;
+    let y2 = inputs[5].value;
+    let blurRadius2 = inputs[6].value;
+    let spreadRadius2 = inputs[7].value;
 
-    document.getElementById("box").style.boxShadow = `${shadowBox} ${shadowColor}`;
+    let shadowColor2 = color[1].value;
 
-    result.innerText = `${shadowBox} ${shadowColor}`
+    let isInset2 = checkbox[1].value;
+    if (checkbox[0].checked) {
+        isInset2 = "inset";
+    }
+
+    let x3 = inputs[8].value;
+    let y3 = inputs[9].value;
+    let blurRadius3 = inputs[10].value;
+    let spreadRadius3 = inputs[11].value;
+
+    let shadowColor3 = color[2].value;
+
+    let isInset3 = checkbox[2].value;
+    if (checkbox[0].checked) {
+        isInset3 = "inset";
+    }
+
+    let shadowBox1 = `${x1}px ${y1}px ${blurRadius1}px ${spreadRadius1}px ${isInset1}`;
+
+    let shadowBox2 = `${x2}px ${y2}px ${blurRadius2}px ${spreadRadius2}px ${isInset2}`;
+
+    let shadowBox3 = `${x3}px ${y3}px ${blurRadius3}px ${spreadRadius3}px ${isInset3}`;
+
+    document.getElementById("box").style.boxShadow = `${shadowBox1} ${shadowColor1}, ${shadowBox2} ${shadowColor2}, ${shadowBox3} ${shadowColor3}`;
+
+    result.innerText = `${shadowBox1} ${shadowColor1}, ${shadowBox2} ${shadowColor2}, ${shadowBox3} ${shadowColor3}`
 }
 
 createShadow();
